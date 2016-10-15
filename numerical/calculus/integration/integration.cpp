8ad6744138da1ae5a99ebc05a14770f856477160
Jinjian Zhai <zhaijinjian@gmail.com>
2016-10-15 14:21:12 -0700

    ADd integrator

---
+++ numerical/calculus/integration/integration.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+// helloacm.com
+double integral(double(*f)(double x), double a, double b, int n) {
+    double step = (b - a) / n;  // width of each small rectangle
+    double area = 0.0;  // signed area
+    for (int i = 0; i < n; i ++) {
+        area += f(a + (i + 0.5) * step) * step; // sum up each small rectangle
+    }
+    return area;
+}
+int main()
+{
+    //cout.precision(7);
+    cout << integral(cos, 0, M_PI , 1000000000)<<endl;
+    return 0;
+}
+
+
