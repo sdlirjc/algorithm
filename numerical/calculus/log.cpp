c3c624f72f7df1c1e3affbf3a8c2a1d703359271
Jinjian Zhai <zhaijinjian@gmail.com>
2016-11-11 16:37:01 -0800

    add log

---
+++ numerical/calculus/log.cpp
@@ -0,0 +1,13 @@
+/* log example */
+#include <stdio.h>      /* printf */
+#include <math.h>       /* log */
+
+int main ()
+{
+    double param, result;
+    param = 10;
+    result = log (param);
+    printf ("log(%f) = %f\n", param, result );
+    return 0;
+}
+
